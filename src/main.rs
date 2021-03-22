1e13c4d2f758be240ebe621f21f8ff5f2326ac3e
Akson <Akson.L@outlook.com>
2021-03-22 10:57:05 +0800

    update in 2021-03-22 10:57:05

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,22 @@ use ansi_term::Color;
 fn main() {
     println!("");
     println!("");
-    println!("{}", Color::Green.blink().paint("Have a nice day!"));
+    print!("{}", Color::Red.blink().paint("H"));
+    print!("{}", Color::Green.blink().paint("a"));
+    print!("{}", Color::Yellow.blink().paint("v"));
+    print!("{}", Color::Blue.blink().paint("e"));
+    print!(" ");
+    print!("{}", Color::Purple.blink().paint("a"));
+    print!(" ");
+    print!("{}", Color::Cyan.blink().paint("n"));
+    print!("{}", Color::White.blink().paint("i"));
+    print!("{}", Color::RGB(31, 31, 31).blink().paint("c"));
+    print!("{}", Color::RGB(31, 31, 31).blink().paint("e"));
+    print!(" ");
+    print!("{}", Color::RGB(31, 31, 31).blink().paint("d"));
+    print!("{}", Color::RGB(31, 31, 31).blink().paint("a"));
+    print!("{}", Color::RGB(31, 31, 31).blink().paint("y"));
+    print!("{}", Color::RGB(31, 31, 31).blink().paint("!"));
     println!("");
     println!("");
 }
